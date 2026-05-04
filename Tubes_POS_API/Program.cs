cdc3d255a7fc827279dc9a8b293e53df73b88f3e
zoovasoup <zoovasoup@proton.me>
2026-05-04 10:44:08 +0700

    chore: refine swagger setup

--- Tubes_POS_API/Program.cs
+++ Tubes_POS_API/Program.cs
@@ -1,22 +1,29 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Tubes POS API",
+        Version = "v1",
+        Description = "Backend API untuk sistem POS Food & Beverage."
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Tubes POS API v1");
+        options.DocumentTitle = "Tubes POS API";
+    });
 }
 
 app.UseHttpsRedirection();
